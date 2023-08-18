533ff38a93de965f7a65359d32de1563b9aee3e1
bsho88 <rpgzzang0613@gmail.com>
2023-08-19 00:32:05 +0900

    필요없는 import 제거

--- SeleniumAttendance.py
+++ SeleniumAttendance.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as ec
 from PIL import Image
 import pytesseract
 import io
